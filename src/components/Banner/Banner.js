67d4c752d650c2d3c56ae64f83371c41e7411604
Akshay-Anand-Code <85056404+Akshay-Anand-Code@users.noreply.github.com>
2022-11-05 01:00:53 +0530

    Update Banner.js

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -41,7 +41,7 @@ function Banner() {
               fontFamily: "Montserrat",
             }}
           >
-            Crypto Hunter
+            Crypto Vault
           </Typography>
           <Typography
             variant="subtitle2"
